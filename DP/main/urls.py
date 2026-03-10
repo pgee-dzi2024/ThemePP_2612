f5629e73b455a14ff0944139ee9de427cd1c8dae
Георги Бориков <pgee_dzi2024@abv.bg>
2026-03-10 13:30:18 +0200

    дизайн

--- DP/main/urls.py
+++ DP/main/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('', login_view, name='home_login'),
+    path('login', login_view, name='login'),
+    path('home', message, name='home'),
 ]
--- DP/main/views.py
+++ DP/main/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
 
-def index(request):
-    return render(request, 'main/index.html')
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect, get_object_or_404
+
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
+
+def message(request):
+    return render(request, 'main/message.html')
+
+
+# @csrf_protect
+def login_view(request):
+    next_url = request.GET.get('next') or request.POST.get('next') or 'home'
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm(request)
+
+    # Подавам form за да покажа грешки/values без да променям визията
+    return render(request, 'main/sign-in.html', {'form': form, 'next': next_url})
